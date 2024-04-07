30d54c8690d403d139963e1771c401b772a6a9e8
killgorealpha <killgorealpha@protonmail.com>
2024-04-07 13:31:03 -0700

    new file:   assets/icons/clockfall.png 	new file:   assets/icons/clockfall.png.import 	new file:   assets/icons/clockspring.png 	new file:   assets/icons/clockspring.png.import 	new file:   assets/icons/clocksummer.png 	new file:   assets/icons/clocksummer.png.import 	new file:   assets/icons/clockwinter.png 	new file:   assets/icons/clockwinter.png.import 	new file:   assets/icons/nextturn.aseprite 	new file:   assets/icons/nextturn.png 	new file:   assets/icons/nextturn.png.import 	new file:   assets/sounds/clock-tick.mp3 	new file:   assets/sounds/clock-tick.mp3.import 	new file:   assets/sounds/darkbell.mp3 	new file:   assets/sounds/darkbell.mp3.import 	modified:   main.gd 	modified:   main.tscn

--- main.gd
+++ main.gd
@@ -2,3 +2,101 @@ extends Node2D
 # Hi, Matthew here
 # This is the Node that holds all of the elements in the game. Some scenes might be instantiated separately,
 # but they'll all be united here. The primary game scripts should be written here
+
+var time = 0
+var season = 0
+var year = 0
+
+var water = 0.5
+var pollution = 0.0
+var water_quality = 1
+
+var mountain = 10000
+var snow = 0.5
+var metal = 0
+
+var ampears = 0
+var ampears_produced = 0
+var ampear_quality = 1.0
+var ampear_groves = 1
+
+var bots = 1
+var productivity = 1.0
+
+var win_progress = 0
+var difficulty = 100
+
+# Win Condition Modifier
+var winmod = 0
+# Win Condition Difficulty Modifier
+var windiffmod = 0
+# Water Modifier
+var watermod = 0
+# Pollution Modifier
+var pollutionmod = 0
+# Water Quality Modifier
+var waterqualmod = 0
+# Mountain Modifier
+var mountainmod = 0
+# Snow Modifier
+var snowmod = 0
+# Metal Modifier
+var metalmod = 0
+# Ampear Modifier
+var ampearmod = 0
+# Ampear Quality Modifier
+var ampearqualmod = 0
+# Ampears Produced Modifier
+var ampearprodmod = 0
+# Ampear Grove Modifier
+var grovemod = 0
+# Bots Modifier
+var botsmod = 0
+# Productivity Modifier
+var prodmod = 0
+
+var clocksummer = preload("res://assets/icons/clocksummer.png")
+var clockfall = preload("res://assets/icons/clockfall.png")
+var clockwinter = preload("res://assets/icons/clockwinter.png")
+var clockspring = preload("res://assets/icons/clockspring.png")
+
+func _process(delta):
+	season = time % 4
+	year = time / 4
+	difficulty += windiffmod
+	water += watermod
+	pollution += pollutionmod
+	water_quality = ((water - (water * pollution)) / water) + waterqualmod
+	ampear_quality = water_quality + ampearqualmod
+	productivity = (ampear_quality + water_quality) / 2 + prodmod
+	$CanvasLayer/TopBar/WinProgress/MarginContainer/ProgressBar.value = win_progress
+	$CanvasLayer/TopBar/ResourceTracker/ResourceContainer/MarginContainer/VFlowContainer/WaterUIGrid/WaterLabel.text = str(water*100) + "%"
+	$CanvasLayer/TopBar/ResourceTracker/ResourceContainer/MarginContainer/VFlowContainer/MountainUIGrid/MountainLabel.text = str(snow*100) + "%"
+	$CanvasLayer/TopBar/ResourceTracker/ResourceContainer/MarginContainer/VFlowContainer/MetalUIGrid/MetalLabel.text = str(metal)
+	$CanvasLayer/TopBar/ResourceTracker/ResourceContainer/MarginContainer/VFlowContainer/AmpearUIGrid/AmpearLabel.text = str(ampears)
+	$CanvasLayer/TopBar/ResourceTracker/ResourceContainer/MarginContainer/VFlowContainer/BotsUIGrid/BotsLabel.text = str(bots)
+	match season:
+		0:
+			$CanvasLayer/TopBar/DateContainer/MarginContainer/CurrentTime.text = "Summer, Year " + str(year)
+			$CanvasLayer/TopBar/Clock.texture = clocksummer
+		1:
+			$CanvasLayer/TopBar/DateContainer/MarginContainer/CurrentTime.text = "Fall, Year " + str(year)
+			$CanvasLayer/TopBar/Clock.texture = clockfall
+		2:
+			$CanvasLayer/TopBar/DateContainer/MarginContainer/CurrentTime.text = "Winter, Year " + str(year)
+			$CanvasLayer/TopBar/Clock.texture = clockwinter
+		3:
+			$CanvasLayer/TopBar/DateContainer/MarginContainer/CurrentTime.text = "Spring, Year " + str(year)
+			$CanvasLayer/TopBar/Clock.texture = clockspring
+
+func _on_end_turn_pressed():
+	time += 1
+	bots += botsmod
+	mountain -= (bots * productivity) + mountainmod
+	ampears_produced = (bots * productivity) * ampear_groves + ampearprodmod
+	ampears +=  ampears_produced - bots + ampearmod
+	win_progress += ((bots * productivity)/difficulty) + winmod
+	if season == 3:
+		$CanvasLayer/TopBar/Clock/ClockBell.play()
+	else:
+		$CanvasLayer/TopBar/Clock/ClockTick.play()
