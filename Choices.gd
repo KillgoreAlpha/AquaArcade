77e5f1305374cfebf0ae5cb10ee4cd2f7b3d8f1d
SporiumCorium <spore2hero@gmail.com>
2024-04-07 13:54:21 -0700

    connected choices to its buttons

--- Choices.gd
+++ Choices.gd
@@ -3,9 +3,21 @@ extends CenterContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_choice_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_choice_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_choice_3_pressed():
+	pass # Replace with function body.
