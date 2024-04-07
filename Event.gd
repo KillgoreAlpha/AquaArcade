d1acb9e667e404cfb974c40b2e97777819c8c105
SporiumCorium <spore2hero@gmail.com>
2024-04-07 14:27:18 -0700

    Update Event.gd

--- Event.gd
+++ Event.gd
@@ -1,9 +1,11 @@
 extends CenterContainer
 var eventID
-
+var eventDescription
+var eventChoice1
+var eventChoice2
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,3 +19,4 @@ func _on_texture_button_1_pressed():
 
 func _on_texture_button_2_pressed():
 	pass # Replace with function body.
+
