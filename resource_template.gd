ead072796915d915cbe2a45e81623c0bdf14d52b
SporiumCorium <spore2hero@gmail.com>
2024-04-07 07:55:51 -0700

    added choice & result windows

--- resource_template.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-# This is an instantiated scene that would hold all the parts of the event popup
-# Repurposed the Element manager so that I already have something to work with
-
-func _ready():
-	show()
